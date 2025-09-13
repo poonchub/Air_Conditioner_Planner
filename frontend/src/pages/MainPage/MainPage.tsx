61a55ea533b6bdf7d7a7772d9815ce41d9e33827
Namzxn <141898614+poonchub@users.noreply.github.com>
2025-09-13 21:57:06 +0700

    update

--- frontend/src/pages/MainPage/MainPage.tsx
+++ frontend/src/pages/MainPage/MainPage.tsx
@@ -14,10 +14,15 @@ import {
 } from "@chakra-ui/react";
 import { useEffect, useState } from "react";
 import "./MainPage.css";
-import { AirVent, FileText, Home, MapPin } from "lucide-react";
+import { AirVent, FileText, HelpCircle, Home, MapPin } from "lucide-react";
 import {
     Chip,
     Collapse,
+    Dialog,
+    DialogActions,
+    DialogContent,
+    DialogContentText,
+    DialogTitle,
     FormControl,
     MenuItem,
     OutlinedInput,
@@ -1977,7 +1982,7 @@ function MainPage() {
         // หน่วยไฟฟ้าต่อวัน
         const start = Number(formData.startTime);
         const end = Number(formData.endTime);
-        
+
         let hoursUsed = end - start;
         if (hoursUsed <= 0) {
             hoursUsed += 24; // กรณีข้ามวัน
@@ -2206,10 +2211,33 @@ function MainPage() {
         console.log("Form Data:", data);
     };
 
+    const [openTimeDescription, setOpenTimeDescription] = useState(false)
+
     return (
         <Box className="main-page-container">
             {/* <Button onClick={handleClickCalculateAll}>Calculate</Button>
             <Button onClick={handleClickCalculateQTotalAll}>Calculate All</Button> */}
+
+            {/* Time Description */}
+            <Dialog
+                open={openTimeDescription}
+                onClose={() => setOpenTimeDescription(false)}
+            >
+                <DialogTitle>
+                    {"คำอธิบาย"}
+                </DialogTitle>
+                <DialogContent>
+                    <DialogContentText sx={{ color: '#000' }}>
+                        เวลาที่ใช้งานมีผลต่อการคำนวณขนาด BTU หากต้องการขนาด BTU ที่ใช้ได้ในทุกช่วงเวลา แนะนำระบุให้อยู่ในช่วงเวลา 8:00-18.00น. ตามจำนวนเวลาที่ใช้งาน
+                    </DialogContentText>
+                </DialogContent>
+                <DialogActions>
+                    <Button onClick={() => setOpenTimeDescription(false)} autoFocus>
+                        ปิด
+                    </Button>
+                </DialogActions>
+            </Dialog>
+
             <Box
                 width={"100%"}
                 padding={"5rem 2rem"}
@@ -2420,7 +2448,11 @@ function MainPage() {
                                             </GridItem>
                                             <GridItem colSpan={1}>
                                                 <Field.Root>
-                                                    <Field.Label>ช่วงเวลาที่ใช้งาน</Field.Label>
+                                                    <Field.Label>
+                                                        ช่วงเวลาที่ใช้งาน
+                                                        <HelpCircle size={16} style={{ minWidth: '16px', minHeight: '16px' }} onClick={() => setOpenTimeDescription(true)} />
+                                                    </Field.Label>
+
                                                     <Grid templateColumns="repeat(2, 1fr)" width={"100%"} gap={3}>
                                                         {/* เวลาเริ่มต้น */}
                                                         <GridItem colSpan={1}>
@@ -4576,7 +4608,7 @@ function MainPage() {
                                                     return (
                                                         <Box key={index}>
                                                             <Box>
-                                                                <Text marginBottom={1}>ตำแหน่งที่ {index + 1}</Text>
+                                                                <Text marginBottom={1}>สำดับที่ {index + 1}</Text>
                                                             </Box>
                                                             <Box
                                                                 display={'flex'}
